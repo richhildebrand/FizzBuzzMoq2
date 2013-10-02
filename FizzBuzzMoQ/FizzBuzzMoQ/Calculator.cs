cf3f0652b8124924c5fc67a8786f4485abc2d1a5
Rich Hildebrand <richhildebrand@gmail.com>
2013-10-01 22:28:35 -0400

    Red - IsDivisibleByThreeShould ReturnFalseIfNumberIsNotDivisibleByThree

---
+++ FizzBuzzMoQ/FizzBuzzMoQ/Calculator.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Linq;
+
+namespace FizzBuzzMoQ
+{
+    public class Calculator
+    {
+        public bool IsDivisibleByThree(int number)
+        {
+            return true;
+        }
+    }
+}
---
+++ FizzBuzzMoQ/Tests/CalculatorTests/IsDivisibleByThreeShould.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Linq;
+using FizzBuzzMoQ;
+using NUnit.Framework;
+
+namespace Tests.CalculatorTests
+{
+    [TestFixture]
+    public class IsDivisibleByThreeShould
+    {
+        [Test]
+        public void ReturnTrueIfNumberIsDivisibleByThree() {
+            var calculator = new Calculator();
+            var isDivisibleByThree = calculator.IsDivisibleByThree(9);
+
+            Assert.IsTrue(isDivisibleByThree);
+        }
+
+        [Test]
+        public void ReturnFalseIfNumberIsNotDivisibleByThree()
+        {
+            var calculator = new Calculator();
+            var isDivisibleByThree = calculator.IsDivisibleByThree(4);
+
+            Assert.IsFalse(isDivisibleByThree);
+        }
+    }
+}
