f2a7adb0515020d07d567084c0622cb0cf57092b
Alderis <alderisopranta@gmail.com>
2013-10-10 09:00:05 -0400

    Red - GetBuzzIfRequired - 2 tests

---
+++ FizzBuzzMoQ/Tests/WordGetterTests/GetBuzzIfRequiredShould.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Linq;
+using FizzBuzzMoQ;
+using Moq;
+using NUnit.Framework;
+
+namespace Tests.WordGetterTests
+{
+    [TestFixture]
+    class GetBuzzIfRequiredShould
+    {
+        [Test]
+        public void ReturnBuzzIfNumberIsDivisibleByThree()
+        {
+            var mockedCalculator = new Mock<ICalculator>();
+            var wordGetter = new WordGetter(mockedCalculator.Object);
+
+            mockedCalculator.Setup(mc => mc.IsDivisibleByFive(It.IsAny<int>())).Returns(true);
+            var result = wordGetter.GetBuzzIfRequired(7);
+
+            Assert.AreEqual(result, "Buzz");
+        }
+
+        [Test]
+        public void ReturnNumberIfNumberIsNotDivisibleByThree()
+        {
+            var mockedCalculator = new Mock<ICalculator>();
+            var wordGetter = new WordGetter(mockedCalculator.Object);
+
+            mockedCalculator.Setup(mc => mc.IsDivisibleByFive(It.IsAny<int>())).Returns(false);
+
+            var result = wordGetter.GetBuzzIfRequired(7);
+
+            Assert.AreNotEqual(result, "Buzz");
+        }
+    }
+}
